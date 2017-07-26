c9cb8a71d86659d94f71b790cda1ba820bbe733a
Breno Paiva <breno.paiva93@gmail.com>
2017-07-26 17:54:48 -0400

    display current time in ny and pr

--- scripts.js
+++ scripts.js
@@ -1,2 +1,7 @@
-let time = Date.now
-$("#time").html(time)
+
+let nyt = new Date().toLocaleTimeString("en-US", {timeZone: "America/New_York"})
+let prt = new Date().toLocaleTimeString("en-US", {timeZone: "Europe/Paris"})
+$("#time").load("time.html", function(){
+  $("#NYC-time").html(nyt);
+  $("#Paris-time").html(prt);
+});
