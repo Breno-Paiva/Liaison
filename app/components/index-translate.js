0b5d91a1e811f03c6dade88527088788a30b2466
Breno Paiva <breno.paiva93@gmail.com>
2017-08-01 13:25:05 -0400

    change translate name

--- app/components/index-translate.js
+++ app/components/index-translate.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
+import config from '../config/environment';
+var translateKey = config.translateKey
 
 export default Ember.Component.extend({
   classNames: ['translate'],
   actions: {
     translate(fromLang, toLang, val){
       let info = "translate " + val + " in " + fromLang + " to " + toLang
-      console.log(info)
-      console.log(to)
-      let to = '.' + toLang
-      $(to).val(info)
+      let toLangClass = '.' + toLang
+      console.log(translateKey)
+      $(toLangClass).val(info)
     }
   }
 });
